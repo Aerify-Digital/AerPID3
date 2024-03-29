418f061d555d0c307716dc2f5b8c4d8cfcdbf2c4
Jon <squid@sqdmc.net>
2024-03-29 04:08:14 -0400

    add version detail

---
+++ src/version.h
@@ -0,0 +1,15 @@
+#ifndef AER_VERSION_H
+#define AER_VERSION_H
+
+// ======================
+// VERSION
+// ======================
+#define VER_MAJOR 3
+#define VER_MINOR 0
+#define VER_BUILD 1
+
+#define VER_WEB_MAJOR 0
+#define VER_WEB_MINOR 3
+#define VER_WEB_BUILD 66
+
+#endif
