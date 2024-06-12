1368397f54a8a9b4c3364d673ecf12bf305d08e0
Jon <squid@sqdmc.net>
2024-06-11 23:02:46 -0400

    increment version build

--- src/version.h
+++ src/version.h
@@ -6,10 +6,10 @@
 // ======================
 #define VER_MAJOR 3
 #define VER_MINOR 1
-#define VER_BUILD 0
+#define VER_BUILD 1
 
 #define VER_WEB_MAJOR 0
 #define VER_WEB_MINOR 3
-#define VER_WEB_BUILD 68
+#define VER_WEB_BUILD 69
 
 #endif
