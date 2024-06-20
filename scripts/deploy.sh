535998380ca33dc426adca5ae187be9f7f03a52b
Jon <squid@sqdmc.net>
2024-06-20 01:54:21 -0400

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -23,8 +23,10 @@ echo -e "${yellow}Aggregating 'aerpid' ...${nc}"
 mkdir ./scripts/build/aerpid/
 cp .pio/build/aerpid/bootloader.bin ./scripts/build/aerpid/bootloader.bin
 cp .pio/build/aerpid/partitions.bin ./scripts/build/aerpid/partitions.bin
+cp .pio/build/aerpid/spiffs.bin ./scripts/build/aerpid/spiffs.bin
 cp .pio/build/aerpid/firmware.bin ./scripts/build/aerpid/firmware.bin
-cp ./scripts/deps/boot_app0.bin  ./scripts/build/aerpid/boot_app0.bin
+cp ./scripts/deps/boot_app0.bin ./scripts/build/aerpid/boot_app0.bin
+cp ./aerpid_spiffs.csv ./scripts/build/aerpid/aerpid_spiffs.csv
 
 cd ./scripts
 cd ./build/aerpid/
@@ -40,8 +42,10 @@ echo -e "${yellow}Aggregating 'aerpid-hp' ...${nc}"
 mkdir ./scripts/build/aerpid-hp/
 cp .pio/build/aerpid-hp/bootloader.bin ./scripts/build/aerpid-hp/bootloader.bin
 cp .pio/build/aerpid-hp/partitions.bin ./scripts/build/aerpid-hp/partitions.bin
+cp .pio/build/aerpid-hp/spiffs.bin ./scripts/build/aerpid-hp/spiffs.bin
 cp .pio/build/aerpid-hp/firmware.bin ./scripts/build/aerpid-hp/firmware.bin
-cp ./scripts/deps/boot_app0.bin  ./scripts/build/aerpid-hp/boot_app0.bin
+cp ./scripts/deps/boot_app0.bin ./scripts/build/aerpid-hp/boot_app0.bin
+cp ./aerpid_spiffs.csv ./scripts/build/aerpid-hp/aerpid_spiffs.csv
 
 cd ./scripts
 cd ./build/aerpid-hp/
@@ -63,8 +67,10 @@ build)
 cd ../
 echo -e "${yellow}Building env 'aerpid' ...${nc}"
 pio run -e aerpid
+pio run -e aerpid --target buildfs
 echo -e "${yellow}Building env 'aerpid-hp' ...${nc}"
 pio run -e aerpid-hp
+pio run -e aerpid-hp --target buildfs
 echo -e "${green}Build Complete!!${nc}"
 sleep 1
 ;;
