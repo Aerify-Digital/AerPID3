adf8a86846437f210794eead3b190e346a14fcec
Jon <squid@sqdmc.net>
2024-09-19 10:17:05 -0400

    update version for test

--- src/version.h
+++ src/version.h
@@ -5,11 +5,11 @@
 // VERSION
 // ======================
 #define VER_MAJOR 3
-#define VER_MINOR 2
-#define VER_BUILD 7
+#define VER_MINOR 3
+#define VER_BUILD 1
 
 #define VER_WEB_MAJOR 0
 #define VER_WEB_MINOR 3
-#define VER_WEB_BUILD 79
+#define VER_WEB_BUILD 80
 
 #endif
