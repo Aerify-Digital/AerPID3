6482bf4687eac6f22cae93c7ebb6e966f72b6c82
squidicuzz <squid@cryptech.services>
2026-01-30 17:51:47 -0500

    Increment build version; Update copyright year

--- src/version.h
+++ src/version.h
@@ -6,12 +6,12 @@
 // ======================
 #define VER_MAJOR 3
 #define VER_MINOR 3
-#define VER_BUILD 17
+#define VER_BUILD 18
 
 #define VER_WEB_MAJOR 0
 #define VER_WEB_MINOR 3
 #define VER_WEB_BUILD 85
 
-#define VER_BUILD_YEAR "2025"
+#define VER_BUILD_YEAR "2026"
 
 #endif
