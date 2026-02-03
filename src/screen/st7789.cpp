3464549762f911d075a2b847fa7d2654f7b3546d
squidicuzz <squid@cryptech.services>
2026-02-02 23:03:01 -0500

    fix load splash message

--- src/screen/st7789.cpp
+++ src/screen/st7789.cpp
@@ -126,8 +126,7 @@ void AerST7789::showSplashPost()
     tft->setTextSize(2);
     tft->setCursor(xt, yt + 28);
     tft->print("(C)");
-    const char *year = String(VER_BUILD_YEAR).c_str();
-    tft->println(year);
+    tft->println(VER_BUILD_YEAR);
 
     delay(1000);
 }
